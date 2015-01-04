87db9828a6c2f098fd6e194826463cc3b89ebaad
matteomanzo <imatteo.manzo@gmail.com>
2015-01-04 17:19:53 +0000

    added session2

---
+++ session2/challenge/2_input_output_control.rb
@@ -0,0 +1,38 @@
+# Prompt the user for a number, then read it in and print out "hi" that many times
+# 
+# Repeat this process until the user submits "bye", then say "goodbye" and end the program
+# HINT: Check out example 2 if you get stuck
+
+# example:
+# PROGRAM: Enter a number
+# USER:    4
+# PROGRAM: hi hi hi hi
+# PROGRAM: Enter a number
+# USER:    2
+# PROGRAM: hi hi
+# PROGRAM: Enter a number
+# USER:    bye
+# PROGRAM: goodbye
+
+
+# remember you can try your program out with              $ ruby 2_input_output_control.rb
+# and when you think it is correct, you can test it with  $ rake 2:2
+
+def hi_hi_goodbye
+  hi = gets.chomp
+  if hi == 'bye'
+  	puts 'goodbye'
+  	exit
+  else
+	n = hi.to_i
+	puts 'hi ' * n
+end
+end
+
+# It's working but fails the test
+
+
+# This will just invoke the method if you run this program directly
+# This way you can try it out by running "$ ruby 2_input_output_control.rb" 
+# but it will still work for our tests
+hi_hi_goodbye if $0 == __FILE__
